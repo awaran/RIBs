1f0958ac46c32d0fd5b851dbc26e0fe625f73d61
awaran <arjay@photokeeper.com>
2019-04-29 21:09:17 -0700

    tutorial 1 and 2

---
+++ ios/tutorials/tutorial1/TicTacToe/LoggedOut/LoggedOutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoggedOutViewController.swift
+//  TicTacToe
+//
+//  Created by Arjay Waran on 4/29/19.
+//  Copyright © 2019 Uber. All rights reserved.
+//
+
+import RIBs
+import RxSwift
+import UIKit
+
+protocol LoggedOutPresentableListener: class {
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
+}
+
+final class LoggedOutViewController: UIViewController, LoggedOutPresentable, LoggedOutViewControllable {
+
+    weak var listener: LoggedOutPresentableListener?
+}
---
+++ ios/tutorials/tutorial2/TicTacToeTests/Root/RootRouterTests.swift
@@ -0,0 +1,9 @@
+//
+//  RootRouterTests.swift
+//  TicTacToeTests
+//
+//  Created by Arjay Waran on 4/29/19.
+//  Copyright © 2019 Uber. All rights reserved.
+//
+
+import Foundation
